f03b804dc4d865d42d4212872c6d522d347e6bc4
Faraz shafi <farazpachu777@gmail.com>
2026-04-22 22:42:29 +0530

    fix someissue

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.pravatar.cc" },
